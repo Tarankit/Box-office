fc3c8a7c8f88217787b3e7101eb742e21cc827c1
Tarankit chadha <tarankitchadha520@gmail.com>
2021-03-01 12:02:20 +0530

    Updates

---
+++ precache-manifest.103907b743a8779dc6abd796019f5f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d56337061e9189856a67fd8c94ac4b",
+    "url": "/Box-office/index.html"
+  },
+  {
+    "revision": "da091a456c7daa66dc79",
+    "url": "/Box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "6d251cee1a3f566be549",
+    "url": "/Box-office/static/js/2.4f211ef8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-office/static/js/2.4f211ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da091a456c7daa66dc79",
+    "url": "/Box-office/static/js/main.0f1e8f48.chunk.js"
+  },
+  {
+    "revision": "04694f4ae56ba9591ca9",
+    "url": "/Box-office/static/js/runtime-main.b4350790.js"
+  }
+]);
